59176fe2bcf5537688ca3bcf8858215c64e04f51
Gavrilko Sergei <seregagavrilko@mail.ru>
2016-12-23 02:40:24 +0300

    sync

--- public/mechanics/gameLauncher.js
+++ public/mechanics/gameLauncher.js
@@ -1,7 +1,9 @@
 (function () {
   const MessageHandler = require('./messageHandler').MessageHandler;
   //const Scene = require('./scene').Scene;
-  const Game = require('./game').Game;
+  const game = require('./game').game;
+  const Event = require('./event').Event;
+
 
   class GameLauncher {
     constructor(canvas) {
@@ -10,17 +12,25 @@
     }
 
     launch() {
-      this.socket = new WebSocket('ws://37.204.2.4:8080/gameapi');
+      this.socket = new WebSocket('ws://37.204.2.4:8081/gameapi');
+      this.event = new Event(this.socket);
 
       this.socket.onmessage = (event) => {
-        this.messageHandler(event.data);
+        this.messageHandler.handle(event.data);
       };
 
       this.socket.onerror = (error) => {
         alert(`Ошибка ${error.message}`);
       };
 
-      this.game = new Game(this.canvas);
+      this.socket.onopen = () => {
+        var message = {username: "arid995", id: 19};
+    		var json = JSON.stringify(message);
+    		this.event.send("ready", json);
+      };
+
+      game.init(this.canvas, this.socket);
+      this.messageHandler.setGame(game);
     }
 
     stop() {
